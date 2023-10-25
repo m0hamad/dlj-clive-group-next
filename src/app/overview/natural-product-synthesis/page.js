7a40c71d8cf076145923de6b40cb6f87aef60984
m0hamad <mohamad.jamaleddine@outlook.com>
2023-10-24 23:28:02 -0600

    Ready

--- src/app/overview/natural-product-synthesis/page.js
+++ src/app/overview/natural-product-synthesis/page.js
@@ -18,7 +18,7 @@ const NaturalProductSynthesis = () => {
                 <div className='nps-url-underline' />
             </Link>
             <h3 className='nps-h3' >Research on Natural Product Synthesis</h3>
-            <p className='nps-h3-text' >The Group's reputation in this area is based on the completed total syntheses of a large number of complex targets. These compounds, many of which are shown below, represent significant challenges to synthetic chemistry, and the research has usually led to the discovery of useful general reagents and methods relevant to the construction of many other types of compounds.</p>
+            <p className='nps-h3-text' >The Group&apos;s reputation in this area is based on the completed total syntheses of a large number of complex targets. These compounds, many of which are shown below, represent significant challenges to synthetic chemistry, and the research has usually led to the discovery of useful general reagents and methods relevant to the construction of many other types of compounds.</p>
             <hr className='nps-hr' />
             {
                 data.map((row, idx) =>
