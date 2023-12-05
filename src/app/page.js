fd0574b5119bb00dd1576d14ef585ca77e054faf
m0hamad <mohamad.jamaleddine@outlook.com>
2023-12-04 21:26:12 -0700

    Adressed latest feedback

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,36 @@ export default function Home() {
 
   const { windowWidth } = useContext(AppContext)
   return (
-    <div>
-      <Banner isPrimary={true} text="The D.L.J. Clive Group" />
-      Home
-    </div>
+    <div style={{
+      display: 'flex',
+      height: 'calc(100vh - 5rem)',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }} >
+      <div
+        style={{
+          backgroundColor: '#164280',
+          borderRadius: '1rem',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '2rem',
+          backgroundImage: "url('/images/background-2.png')",
+          backgroundSize: '24rem',
+          backgroundRepeat: 'repeat',
+          boxSizing: 'border-box',
+          width: '100%'
+        }}
+      >
+        <p style={{
+          margin: 'auto',
+          color: '#FFF',
+          fontWeight: '500',
+          fontSize: '2.75rem',
+          lineHeight: '3rem',
+          textAlign: "center"
+        }} >The D.L.J. Clive Group</p>
+      </div>
+      {/* <Banner isPrimary={true} text="The D.L.J. Clive Group" /> */}
+    </div >
   )
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,32 @@
 'use client'
-import React from 'react'
+import React, { useContext } from 'react'
 import './layout.css'
 import Footer from '../Footer/Footer'
 import NavBar from '../NavBar/NavBar'
 import ScrollUpButton from '../ScrollUpButton/ScrollUpButton'
+import AppContext from '@/context/AppContext'
 
 const Layout = ({ children }) => {
+
+    const { pathname } = useContext(AppContext)
+    console.log('pathname', pathname)
+
     return (
-        <div className='layout' >
+        <div
+            className='layout'
+            style={{
+                backgroundImage: pathname === '/' ? "url('/images/background-3.png')" : "url('/images/background.png')",
+                backgroundSize: '16rem',
+                backgroundRepeat: 'repeat'
+            }}
+        >
             <NavBar />
             <section className='section'>
                 {children}
             </section>
             <ScrollUpButton />
             <Footer />
-        </div>
+        </div >
     )
 }
 
